6d2eb9e02940b828739073662274e7bbb434948d
heeje Shin <shjs6711@gmail.com>
2025-07-03 21:05:48 +0900

    Revert "feat: 모달 컨텍스트 시스템 구현 및 글쓰기 기능 추가"

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
-
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-import AppProvider from "@/providers/AppProvider";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -29,9 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <AppProvider>
-          {children}
-        </AppProvider>
+        {children}
       </body>
     </html>
   );
--- contexts/ModalContext.tsx
+++
@@ -1,5 +0,0 @@
-'use client'
-import React, { createContext } from 'react';
-import { ModalContextType } from '@/types/modal.type';
-
-export const ModalContext = createContext<ModalContextType | undefined>(undefined);
